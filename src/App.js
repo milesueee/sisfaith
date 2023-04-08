24bee1efc6bf06ba5c5eab01dfe9fcd361310de5
Milesueee <mikoybailon250@gmail.com>
2023-04-08 15:48:25 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
-function App() {
+export default function Home() {
+  const [darkMode, setDarkMode] = useState(false)
+  const [github_src, setGithubSrc] = useState('https://i.imgur.com/qkgfyLX.png')
+
+
+  const joy = [
+    'https://i.imgur.com/NGYz7eM.jpg',
+    'https://i.imgur.com/Yu5n5ti.jpg',
+    'https://i.imgur.com/r0Y6stC.jpg',
+    'https://i.imgur.com/BEqypvj.jpg',
+    'https://i.imgur.com/4ceKCla.jpg',
+    'https://i.imgur.com/eFCicxJ.jpg',
+    'https://i.imgur.com/X75QP2k.jpg',
+    'https://i.imgur.com/xUqFGI3.jpg',
+    'https://i.imgur.com/RNfLfOv.jpg'
+  ]
+
+  useEffect(() => {
+    if (darkMode) {
+      setGithubSrc('https://i.imgur.com/AwgrBOi.png')
+    } else {
+      setGithubSrc('https://i.imgur.com/qkgfyLX.png')
+    }
+  }, [darkMode])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div
+          className={darkMode ? 'container_dark' : 'container_bright'}
+      >
+        <DarkModeSwitch
+            style={{ marginBottom: '2rem' }}
+            checked={darkMode}
+            onChange={setDarkMode}
+            size={40}
+        />
 
-export default App;
+        <div className='main-div'>
+          <h1>Welcome to your website, Joy</h1>
+          <h2>Stay as long as you'd like</h2>
+        </div>
+
+
+        <div className='social-media_container'
+        >
+          <a href='https://github.com/milesueee'>
+            <img className='social-media_icon' src={github_src} alt='github' />
+          </a>
+        </div>
+
+        <div className='joy-container'>
+          <h2 className='joy-title'>
+            here are some of ur coteee pics :{'>'}
+          </h2>
+
+          <div >
+            {joy.map((joy, index) => (
+                <img
+                    className='joy'
+                    src={joy}
+                    alt={`joy ${index}`}
+                />
+            ))}
+          </div>
+        </div>
+
+        <footer>There isn't really anything else in here except u soo fineee🤌🤌🤩🤩</footer>
+      </div>
+  )
+}
