6d588555024a8e3017a4875d35d1fd5b4367a331
Milesueee <mikoybailon250@gmail.com>
2023-08-12 21:42:08 +0800

    faith

--- src/App.js
+++ src/App.js
@@ -1,61 +1,70 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Loader from './Loader';
 
-export default function Home() {
-  const [github_src, setGithubSrc] = useState('https://i.imgur.com/qkgfyLX.png')
-
+function App() {
+  const [loading, setLoading] = useState(true);
 
   const joy = [
-    'https://i.imgur.com/NGYz7eM.jpg',
-    'https://i.imgur.com/Yu5n5ti.jpg',
-    'https://i.imgur.com/r0Y6stC.jpg',
-    'https://i.imgur.com/BEqypvj.jpg',
-    'https://i.imgur.com/4ceKCla.jpg',
-    'https://i.imgur.com/eFCicxJ.jpg',
-    'https://i.imgur.com/X75QP2k.jpg',
-    'https://i.imgur.com/xUqFGI3.jpg',
-    'https://i.imgur.com/RNfLfOv.jpg'
-  ]
+    'https://imgur.com/VaBXlrE.jpg',
+    'https://imgur.com/Lbqe15t.jpg',
+    'https://imgur.com/gwm8noy.jpg',
+    'https://imgur.com/rrQfYog.jpg',
+    'https://imgur.com/HV6R2NB.jpg',
+    'https://imgur.com/AOoqwcw.mp4',
+  ];
 
   useEffect(() => {
-  })
+    // Wait for 3 seconds before hiding the loader
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
-      <div
-      >
+    <div>
+      {loading ? <Loader /> : null}
 
-        <div className='main-div'>
-          <h1>Welcome to your website, Joy</h1>
-          <h2>Stay as long as you'd like</h2>
-        </div>
-
-
-        <div className='social-media_container'
-        >
-          <a href='https://github.com/milesueee'>
-            <img className='social-media_icon' src={github_src} alt='github' />
-          </a>
-        </div>
-        <div className='blank'>
-
-        </div>
-        <div className='joy-container'>
-          <h2 className='joy-title'>
-            here are some of ur coteee pics :{'>'}
-          </h2>
-
-          <div className='fadein'>
-            {joy.map((joy, index) => (
-                <img
-                    className='joy'
-                    src={joy}
-                    alt={`joy ${index}`}
-                />
-            ))}
-          </div>
-        </div>
-
-        <footer>There isn't really anything else in here except u soo fineee HAHAHAHAHAHA🤌🤌🤩🤩</footer>
+      <div className="main-div">
+        <h1>Welcome to your website, sis</h1>
+        <h2>Stay as long as you'd like</h2>
       </div>
-  )
+
+      <div className="social-media_container">
+        <a href="https://github.com/milesueee">
+          <img
+            className="social-media_icon"
+            src="https://i.imgur.com/qkgfyLX.png"
+            alt="github"
+          />
+        </a>
+      </div>
+
+      <div className="blank"></div>
+
+      <div className="joy-container">
+        <h2 className="joy-title">pics mo gaga </h2>
+
+          <div className="fadein">
+              {joy.map((item, index) => (
+                  // Check if the item ends with .mp4 to determine if it's a video
+                  item.endsWith('.mp4') ? (
+                          <video className="joy" controls autoPlay loop key={index}>
+                              <source src={item} type="video/mp4" />
+                              Your browser does not support the video tag.
+                          </video>
+                      ) : (
+                          <img className="joy" src={item} alt={`joy ${index}`} />
+                      )
+              ))}
+        </div>
+      </div>
+
+      <footer>
+        ukininam met🤩🤌
+      </footer>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.css';
+
+function Loader() {
+  return (
+    <div className="custom-loader">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Loader;
