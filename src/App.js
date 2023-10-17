daa9c67c2b7d88e4a2fd91e77aea357e37b47725
milesueee <78363277+milesueee@users.noreply.github.com>
2023-10-17 19:36:48 +0800

    giffffffff

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
       </footer>
 
       {/* Add the EasterEgg component with the GIF URL */}
-      <EasterEgg gifUrl="https://imgur.com/fw8xw8F" />
+      <EasterEgg gifUrl="https://imgur.com/fw8xw8F.gif" />
     </div>
   );
 }
